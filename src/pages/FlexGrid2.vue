ef58a2f4d7ee10d0a2f15be1aa97d79ee277316c
jaeyo <jaeyoung645@naver.com>
2023-03-31 02:12:02 +0900

    2023-03-31 02:11 인스타그램 프로필 페이지 만들기1 인프런강의

--- src/pages/FlexGrid2.vue
+++ src/pages/FlexGrid2.vue
@@ -1,8 +1,36 @@
 <template>
-  <q-page class="q-pa-xl">
-    <section class="q-mb-xl">
-      <div class="text-h4">Flex Grid 2</div>
-      <q-separator class="q-my-md" />
+  <q-page padding>
+    <section class="row q-py-lg q-col-gutter-x-xl">
+      <div class="col-auto">
+        <q-avatar size="150px">
+          <q-img src="/logo.png"></q-img>
+        </q-avatar>
+      </div>
+      <div class="col-grow column justify-between">
+        <div class="row justify-between">
+          <span>helinlee.gram</span>
+          <div>
+            <q-btn label="프로필 편집"></q-btn>
+            <q-btn label="설정"></q-btn>
+          </div>
+          </div>
+        <div>
+          <span>게시물 238</span>
+          <span>팔로워 2,032</span>
+          <span>팔로우 1,432</span>
+        </div>
+        <div>
+          <div>헬린이</div>
+          <div>#ESFJ 인스타잠시 쉬어요</div>
+          <div>linktr.ee/gymconding</div>
+        </div>
+      </div>
+    </section>
+    <section class="bg-yellow">
+
+    </section>
+    <section class="bg-green">
+
     </section>
   </q-page>
 </template>
@@ -12,5 +40,8 @@
 </script>
 
 <style lang="scss" scoped>
+.q-page {
+  background-color: #fafafa;
+}
 
 </style>
